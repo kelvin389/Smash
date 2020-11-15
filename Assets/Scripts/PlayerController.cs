37df5eb8bf2382faa9825f038a5018c50c3a2078
kelvin389 <kelvinl.leung@gmail.com>
2020-11-15 00:23:17 -0800

    Break up Update function into several smaller ones

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
+    public CapsuleCollider col;
+
     public float dashSpeed;
     public float runSpeed;
     public float dashLength;
+    public float friction;
 
     public float mass;
     public float jumpForce;
@@ -16,13 +19,14 @@ public class PlayerController : MonoBehaviour
 
     public float airDodgeForce;
     public float airDodgeLength;
-    public float airDodgeSpeed;
+    public float airDodgeStoppingSpeed;
 
     private float dashStartTime = 0f;
 
     private float airDodgeStartTime = 0f;
 
     private int airJumps = 0;
+    private bool inJumpSquat = false;
     private bool grounded = false;
     private bool fastFalling = false;
 
@@ -32,67 +36,32 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        col.material.dynamicFriction = friction;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(rb.velocity);
+        col.material.dynamicFriction = friction;
         if (airDodging)
-        {
-            if (Time.time - airDodgeStartTime >= airDodgeLength || grounded)
-            {
-                helpless = true;
-                rb.useGravity = true;
-                airDodging = false;
-            }
-            else
-            {
-                rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, Time.deltaTime / airDodgeSpeed);
-                return;
-            }
+        {                
+            AirDodgeMove();
+            return; // do not allow further input during an airdodge
         }
 
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        float speed = dashSpeed; // assume we are dashing by default
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (input.x != 0)
+            HorizontalInputHandler(input.x);
+        if (input.y != 0)
+            VerticalInputHandler(input.y);
+
+        if (!helpless)
         {
-            if (grounded)
-            {
-                // shield
-            }
-            else
-            {
-                AirDodge(input);
-                return;
-            }
+            ShieldInputHandler(input);
         }
 
-        // set dashStartTime whenever a movement key is pushed
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A))
-        {
-            dashStartTime = Time.time;
-        }
-        // if we have been moving in one direction for more than dashLength, then we should be at running speed
-        if (Time.time - dashStartTime >= dashLength)
-        {
-            speed = runSpeed;
-        }
-
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            TryJump();
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            if (!grounded && isFalling())
-            {
-                fastFalling = true;
-            }
-        }
-
-        MoveX(input.x, speed);
     }
 
     private void FixedUpdate()
@@ -105,15 +74,78 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void MoveX(float axis, float speed)
+    private void HorizontalInputHandler(float xInput)
     {
+        float speed = dashSpeed;
+
+        // set dashStartTime whenever a movement key is pushed
+        if (Input.GetButtonDown("Horizontal") && (xInput < 0 || xInput > 0))
+        {
+            dashStartTime = Time.time;
+        }
+
+        // if we have been moving in one direction for more than dashLength, then we should be at running speed
+        if (Time.time - dashStartTime >= dashLength)
+        {
+            speed = runSpeed;
+        }
+
         // read current velocity
         Vector3 vel = rb.velocity;
         // modify x velocity
-        vel.x = axis * speed;
+        vel.x = xInput * speed;
 
         // set velocity
         rb.velocity = vel;
+    }
+
+    private void VerticalInputHandler(float yInput)
+    {
+        if (Input.GetButtonDown("Vertical"))
+        {
+            if (yInput > 0) // up
+            {
+                TryJump();
+            }
+            if (yInput < 0) // down
+            {
+                if (isFalling())
+                {
+                    fastFalling = true;
+                }
+            }
+        }
+    }
+
+    private void AirDodgeMove()
+    {
+        if (Time.time - airDodgeStartTime >= airDodgeLength || grounded)
+        {
+            StopAirDodge();
+        }
+        else
+        {
+            rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, Time.deltaTime * airDodgeStoppingSpeed);
+        }
+    }
+
+    private void ShieldInputHandler(Vector2 input)
+    {
+        if (Input.GetButtonDown("Fire3"))
+        {
+            if (grounded)
+            {
+                // shield
+            }
+            else
+            {
+                // if tech
+                // else if l-cancel
+
+                // else
+                AirDodge(input);
+            }
+        }
     }
 
     private void TryJump()
@@ -141,28 +173,41 @@ public class PlayerController : MonoBehaviour
 
     private bool isFalling()
     {
-        return rb.velocity.y <= 0;
+        return rb.velocity.y <= 0 && !grounded;
     }
 
-    private void AirDodge(Vector2 direction)
+    private void AirDodge(Vector2 input)
     {
         rb.useGravity = false; // disable gravity
-        direction *= airDodgeForce;
-        rb.velocity = direction;
+        input *= airDodgeForce; // multiply input by force
+        rb.velocity = input; // overwrite current velocity with airdodge velocity
 
         airDodgeStartTime = Time.time;
         airDodging = true;
+    }
+
+    private void StopAirDodge()
+    {
+        rb.useGravity = true;
+        airDodging = false;
+        helpless = true;
     }
 
     void OnCollisionEnter(Collision theCollision)
     {
         if (theCollision.gameObject.name == "floor")
         {
+            Debug.Log("A");
+            // wavedash
+            if (airDodging) 
+            {
+                StopAirDodge();
+            }
             grounded = true;
             airJumps = maxAirJumps; // restore air jumps when landing
             fastFalling = false;
 
-            helpless = false;
+            helpless = false; // also overrides helplessnes from stoping airdodge
         }
     }
      
